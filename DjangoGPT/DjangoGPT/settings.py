120a3d637d294caa2bab18163458556b618b7526
nabil-mabrouk <nabil.mabrouk.ai@gmail.com>
2023-07-21 19:39:39 +0200

    landing page

--- DjangoGPT/DjangoGPT/settings.py
+++ DjangoGPT/DjangoGPT/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-md$p!)5ve677ylkq!--dbxkhuav3!n8@2dc*x8mm(li*m#%&(j'
-
+#SECRET_KEY = 'django-insecure-md$p!)5ve677ylkq!--dbxkhuav3!n8@2dc*x8mm(li*m#%&(j'
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -58,7 +61,7 @@ ROOT_URLCONF = 'DjangoGPT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,9 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- DjangoGPT/landing/views.py
+++ DjangoGPT/landing/views.py
@@ -1,3 +1,7 @@
+
+
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'landing/index.html')
